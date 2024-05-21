b07fe9f72e35b698fb277b686ca40438689d7da3
David smart <bamideledavidsmart40@gmail.com>
2024-05-21 01:13:56 +0100

    Added nthe getMeaning() and playAudio() function.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useState } from "react";
+import Axios from "axios";
+import "./App.css";
+import { FaSearch } from "react-icons/fa";
+import { FcSpeaker } from "react-icons/fc";
 
-const App = () => {
-  return <div></div>;
-};
+function App() {
+  const [data, setData] = useState("");
+  const [searchWord, setSearchWord] = useState("");
+
+
+  function getMeaning() {
+    Axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`).then((response) =>{
+      setData(response.data[0]);
+    });
+  }
+
+
+  function playAudio(){
+    let audio = new Audio(data.phonetics[0].audio);
+    audio.play();
+  }
+}
 
 export default App;
