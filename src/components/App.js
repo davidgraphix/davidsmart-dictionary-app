20e76f5b511fed5af605fc5c23ccbea14b923ce9
David smart <bamideledavidsmart40@gmail.com>
2024-05-22 17:05:18 +0100

    Update App.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SearchMenu from "./SearchMenu";
+import ShowSearchResult from "./ShowSearchResults";
+import "../App.css"
+
+const App = () => {
+  return (
+    <div>
+      <h1>David Smart Dictionary app</h1>
+      <SearchMenu />
+      <ShowSearchResult />
+    </div>
+  );
+};
+
+export default App;
