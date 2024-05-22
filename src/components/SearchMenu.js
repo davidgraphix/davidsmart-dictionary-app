90f23ec2bcc60aed651889b4cad195c2ebc7d1b0
David smart <bamideledavidsmart40@gmail.com>
2024-05-22 16:58:10 +0100

    Create SearchMenu.js

---
+++ src/components/SearchMenu.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "../App.css";
+import { useState } from "react";
+import Axios from "axios";
+import { FaSearch } from "react-icons/fa";
+
+function SearchMenu() {
+  const [searchWord, setSearchWord] = useState("");
+  const [setData] = useState("");
+
+  function getMeaning() {
+    Axios.get(
+      `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`
+    ).then((response) => {
+      setData(response.data[0]);
+    });
+  }
+
+  return (
+    <div className="searchBox">
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearchWord(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          getMeaning();
+        }}
+      >
+        <FaSearch size="20px" />
+      </button>
+    </div>
+  );
+}
+
+export default SearchMenu;
